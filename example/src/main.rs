07592bbe1d8eae1a83cdcecbd66e57abdd00aee7
Johannes Waigel <johannes@waigel.me>
2020-09-07 02:15:20 +0200

    add example and readme.
    Co-authored-by: Valentin Sickert <lapotor@lapotor.de>

---
+++ example/src/main.rs
@@ -0,0 +1,8 @@
+use healthcard_rust::*;
+
+fn main() {
+    println!("Read data from German public health insurance cards (eGK)");
+    let card = get_card();
+    println!("Generation {:?}", get_card_generation(&card));
+    println!("{:?}", healthcard_rust::get_card_data(&card).to_string())
+}
